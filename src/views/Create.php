f0cc9be25254583ad2e96bbf2ad2cbe38d5ab502
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2024-10-30 11:59:27 +0100

    fix: changed /exercises/new input name to work with app

--- src/views/Create.php
+++ src/views/Create.php
@@ -18,15 +18,15 @@ ob_start();
 <main class="container">
     <h1>New Exercise</h1>
 
-    <form action="/exercises" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="/HfdEImdtV4/XIdB53jcsjYbLUvBfg22I5rFpg0+G6a9Xb+9pk5+50F3o0EontYy3Sl3l97prWQsBeRtIAir7A==" />
+    <form action="/exercises" accept-charset="UTF-8" method="post">
 
         <div class="field">
             <label for="exercise_title">Title</label>
-            <input type="text" name="exercise[title]" id="exercise_title" />
+            <input type="text" name="title" id="exercise_title" />
         </div>
 
         <div class="actions">
-            <input type="submit" name="commit" value="Create Exercise" data-disable-with="Create Exercise" />
+            <input type="submit" name="commit" value="Create Exercise" />
         </div>
     </form>
 </main>
