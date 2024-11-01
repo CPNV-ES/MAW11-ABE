a8884815003fcd4c4ad4a047c48a075edac82d53
Arthur Bottemanne <walvieswisel@gmail.com>
2024-11-01 20:01:41 +0100

    refactor: removed unnecessary table/class

--- db/initDatabase.sql
+++ db/initDatabase.sql
@@ -22,15 +22,15 @@ USE `looper`;
 -- Dumping structure for table looper.answers
 CREATE TABLE IF NOT EXISTS `answers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `contents` text NOT NULL,
   `field_id` int(11) NOT NULL,
   `fulfillment_id` int(11) NOT NULL,
-  `contents` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `field_id` (`field_id`),
   KEY `fulfillment_id` (`fulfillment_id`),
   CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`field_id`) REFERENCES `fields` (`id`),
   CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`fulfillment_id`) REFERENCES `fulfillments` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
 
@@ -47,21 +47,12 @@ CREATE TABLE IF NOT EXISTS `exercises` (
 -- Dumping structure for table looper.fields
 CREATE TABLE IF NOT EXISTS `fields` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(20) NOT NULL DEFAULT '0',
   `type` varchar(50) NOT NULL DEFAULT '',
   `exercises_id` int(11) NOT NULL,
-  `label` text NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `field_type` (`type`) USING BTREE,
   KEY `fields_ibfk_2` (`exercises_id`),
   CONSTRAINT `fields_ibfk_2` FOREIGN KEY (`exercises_id`) REFERENCES `exercises` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table looper.fieldtypes
-CREATE TABLE IF NOT EXISTS `fieldtypes` (
-  `type` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`type`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
