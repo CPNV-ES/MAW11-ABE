2628eda8ca091a5bad402d914b8b1642c746fd63
walvie <walvieswisel@gmail.com>
2024-12-19 21:40:49 +0100

    refactor: fulfillments controller

--- src/Controllers/FulfillmentsController.php
+++ src/Controllers/FulfillmentsController.php
@@ -6,7 +6,6 @@ use App\Models\Answers;
 use App\Models\Fields;
 use App\Models\Exercises;
 use App\Models\Fulfillments;
-
 use Exception;
 
 class FulfillmentsController extends Controller
@@ -15,9 +14,7 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $exercise = Exercises::findBy("id", $data["exercise"]["id"])[0];
-
             $fields = Fields::getFields($data["exercise"]["id"]);
 
             include_once PAGE_DIR . "/AnswerExercise.php";
@@ -31,7 +28,6 @@ class FulfillmentsController extends Controller
         $exerciseId = parent::getModelDataByIds($parameters)["exercise"]["id"];
 
         try {
-
             $fulfillment = Fulfillments::createFulfillment($exerciseId);
             $fulfillmentId = $fulfillment[0]["id"] ?? null;
 
@@ -53,13 +49,9 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $exercise = Exercises::findBy("id", $data["exercise"]["id"])[0];
-
             $fields = Fields::getFields($data["exercise"]["id"]);
-
-            $column = "exercise_id";
-            $fulfillments = Fulfillments::getFulfillmentsWithAnswers($column, $exercise["id"]);
+            $fulfillments = Fulfillments::getFulfillmentsWithAnswers("exercise_id", $exercise["id"]);
 
             include_once PAGE_DIR . "/ExerciseResults.php";
         } catch (Exception $e) {
@@ -71,17 +63,13 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $exercise = $data["exercise"];
-
             $fields = Fields::getFields($exercise["id"]);
-
             $fulfillment = $data["fulfillment"];
 
-            $fields = array_map(function ($field) use ($fulfillment) {
-                $field["answer"] = Answers::findAnswersFromFulfillmentField($fulfillment, $field);
-                return $field;
-            }, $fields);
+            foreach ($fields as &$field) {
+                $field['answer'] = Answers::findAnswersFromFulfillmentField($fulfillment, $field);
+            }
 
             include_once PAGE_DIR . "/EditFulfillment.php";
         } catch (Exception $e) {
@@ -93,17 +81,13 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $exercise = $data["exercise"];
-
             $fields = Fields::getFields($exercise["id"]);
-
             $fulfillment = $data["fulfillment"];
 
-            $fields = array_map(function ($field) use ($fulfillment) {
-                $field["answer"] = Answers::findAnswersFromFulfillmentField($fulfillment, $field);
-                return $field;
-            }, $fields);
+            foreach ($fields as &$field) {
+                $field['answer'] = Answers::findAnswersFromFulfillmentField($fulfillment, $field);
+            }
 
             include_once PAGE_DIR . "/AnswersFulfillment.php";
         } catch (Exception $e) {
@@ -115,9 +99,7 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $exercise = $data["exercise"];
-
             $fulfillments = Fulfillments::findBy("exercise_id", $exercise["id"]);
 
             include_once PAGE_DIR . "/ExerciseFulfillments.php";
@@ -130,9 +112,7 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $exercise = $data["exercise"];
-
             $fulfillment = $data["fulfillment"];
 
             foreach ($_POST["fulfillment"]["answers"] as $key => $answer) {
@@ -149,7 +129,6 @@ class FulfillmentsController extends Controller
     {
         try {
             $data = parent::getModelDataByIds($parameters);
-
             $fulfillment = $data["fulfillment"];
 
             Fulfillments::deleteFulfillmentFromId($fulfillment["id"]);
